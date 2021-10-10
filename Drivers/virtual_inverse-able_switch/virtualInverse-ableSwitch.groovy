b87ba7fc0d2c09946b5137b0b2b9cd64970336ab
jshimota01 <jshimota@e-wiz.com>
2021-10-10 09:12:40 -0700

    Create virtualInverse-ableSwitch.groovy

---
+++ Drivers/virtual_inverse-able_switch/virtualInverse-ableSwitch.groovy
@@ -0,0 +1,60 @@
+/*
+ * Virtual Inverse-able Switch
+ *
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *      Date          Source        Version     What                                              URL
+ *      ----          ------        -------     ----                                              ---
+ *      2021-10-10    jshimota      0.1.0       Starting version
+ */
+metadata {
+    definition(
+            name: "Virtual Inverse-able Switch",
+            namespace: "jshimota",
+            author: "James Shimota",
+            importUrl: "https://raw.githubusercontent.com/jshimota01/hubitat/main/Drivers/virtual_inverse-able_switch/virtualInverse-ableSwitch.groovy"
+    ) {
+        capability "Actuator"
+        capability "Switch"
+    }
+
+    preferences {
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
+        input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
+        input name: "reversed", type: "bool", title: "Reverse Action", defaultValue: false, required: true
+    }
+
+def parse(description) {
+}
+
+def on() {
+    if (reversed) sendEvent(name: "switch", value: "off")
+    if(reversed) sendEvent(name: "switch", value: "on")
+}
+
+def off() {
+    if (reversed) sendEvent(name: "switch", value: "on")
+    if(reversed) sendEvent(name: "switch", value: "off")
+}
+
+def updated(){
+    runIn(1, 'updatedDelayed')  //avoids requiring user to refresh device details page to see the change
+}
+
+
+def installed() {
+    initialize()
+}
+
+def initialize() {
+    off()
+}
