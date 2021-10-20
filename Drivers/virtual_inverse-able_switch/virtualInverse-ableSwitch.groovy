80a4f5ddda7eda3e94489e04588346cb66a85249
jshimota01 <jshimota@e-wiz.com>
2021-10-20 15:55:53 -0700

    Lots of work getting toggle and autoonoff to function

--- Drivers/virtual_inverse-able_switch/virtualInverse-ableSwitch.groovy
+++ Drivers/virtual_inverse-able_switch/virtualInverse-ableSwitch.groovy
@@ -1,5 +1,5 @@
-/**
- * Virtual Inverse-able Switch
+/*
+ * Virtual Inverse-able w-autotoggle Switch
  *
  *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -16,11 +16,12 @@
  *      ----          ------        -------     ----                                              ---
  *      2021-10-10    jshimota      0.1.0       Starting version
  *      2021-10-11    jshimota      0.1.1       Added redundant set using parent and added initialize to follow preference so always starts in off position
- *
- */
+ *      2021-10-17    jshimota      0.1.2       Added Toggle feature
+ *      2021-10-20    jshimota      0.2.0       Added AutoOffOn - will toggle based on device setting in preferences
+ * */
 metadata {
     definition(
-            name: "Virtual Inverse-able Switch",
+            name: "Virtual Inverse-able w-autotoggle Switch",
             namespace: "jshimota",
             author: "James Shimota",
             importUrl: "https://raw.githubusercontent.com/jshimota01/hubitat/main/Drivers/virtual_inverse-able_switch/virtualInverse-ableSwitch.groovy"
@@ -28,13 +29,15 @@ metadata {
         capability "Actuator"
         capability "Switch"
         capability "Initialize"
-        capability "Refresh"
+        command "toggle"
+        command "readCurrentIntoLog"
     }
 
     preferences {
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
         input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
         input name: "reversed", type: "bool", title: "Reverse Action", defaultValue: false, required: true
+        input name: "autoOffOn", type: "enum", description: "Automatically turns off -or- on the device after selected time.", title: "Enable Auto-Off-On", options: [[0:"Disabled"],[1:"1 second"],[2:"2 seconds"],[5:"5 seconds"],[10:"10 seconds"],[15:"15 seconds"],[20:"20 seconds"],[30:"30 seconds"],[45:"45 seconds"],[60:"1 minute"],[120:"2 minutes"],[300:"5 minutes"],[600:"10 minutes"],[900:"15 minutes"],[1200:"20 minutes"],[1800:"30 minutes"],[2700:"45 minutes"],[3200:"1 hour"]], defaultValue: 0
     }
 }
 
@@ -42,36 +45,130 @@ def parse(description) {
 }
 
 def logsOff() {
-    log.warn "debug logging disabled..."
+    log.warn "${device.displayName} debug logging disabled..."
     device.updateSetting("logEnable", [value: "false", type: "bool"])
 }
 
 def on() {
-    if (reversed) sendEvent(name: "switch", value: "off")
-    if (reversed) state.device = false
-    parent?.componentOff(this.device)
-    if (!reversed) sendEvent(name: "switch", value: "on")
-    if (!reversed) state.device = true
-    parent?.componentOn(this.device)
+    sendEvent(name: "switch", value: "on", isStateChange: true)
+    if (reversed) {
+        state.device = false
+        parent?.componentOff(this.device)
+        if (txtEnable) log.info "${device.displayName} switch turned ON - is reversed - device state is $state.device."
+    } else {
+        state.device = true
+        state.autoOffOnFired = false
+        parent?.componentOn(this.device)
+        if (txtEnable) log.info "${device.displayName} switch turned ON - is NOT reversed - device state is $state.device."
+    }
+    autotoggle()
 }
 
 def off() {
-    if (reversed) sendEvent(name: "switch", value: "on")
-    if (reversed) state.device = true
-    parent?.componentOn(this.device)
-    if (!reversed) sendEvent(name: "switch", value: "off")
-    if (!reversed) state.device = false
-    parent?.componentOff(this.device)
+    sendEvent(name: "switch", value: "off", isStateChange: true)
+    if (reversed) {
+        state.device = true
+        state.autoOffOnFired = false
+        parent?.componentOn(this.device)
+        if (txtEnable) log.info "${device.displayName} switch turned OFF - is reversed - device state is $state.device."
+    } else {
+        state.device = false
+        parent?.componentOff(this.device)
+        if (txtEnable) log.info "${device.displayName} switch turned OFF - is NOT reversed - device state is $state.device."
+    }
+    autotoggle()
+}
+
+def autotoggle() {
+    if (logEnable) log.debug "autotoggle: AutoOffOnFired prerun is $state.autoOffOnFired"
+    if (!state.autoOffOnFired == true) {
+        if (logEnable) log.debug "autotoggle: Auto-Off-OnFired innotloop is $state.autoOffOnFired"
+        if (autoOffOn.toInteger() > 0) {
+            if (logEnable) log.debug "${device.displayName} will toggle in $autoOffOn seconds."
+            runIn(autoOffOn.toInteger(), toggle)
+            state.autoOffOnFired = true
+            if (logEnable) log.debug "autotoggle: Auto-Off-OnFired afterrun is $autoOffOnFired"
+        }
+    }
+}
+
+def toggle() {
+    if (reversed) {
+        if (state.device == true) {
+            on()
+        } else {
+            off()
+        }
+    } else {
+        if (!reversed) {
+            if (state.device == false) {
+                on()
+            } else {
+                off()
+            }
+        }
+    }
 }
 
 def updated(){
+    log.info "${device.displayName} updated..."
+    log.warn "${device.displayName} debug logging is: ${logEnable == true}"
+    log.warn "${device.displayName} description logging is: ${txtEnable == true}"
+    initialize()
 }
 
 def installed() {
     initialize()
 }
 
+def readCurrentIntoLog() {
+    if (txtEnable) log.info "Read Current button pushed."
+    if (txtEnable) log.info "Read Current: device state is $state.device."
+    if (txtEnable) log.info "Read Current: device inverted? $reversed."
+    if (logEnable) log.info "Read Current: Auto-Off-OnFired is $state.autoOffOnFired"
+    if (autoOffOn.toInteger() > 0) {
+        if (txtEnable) log.info "Read Current: AutoOffOn will toggle in $autoOffOn seconds"
+    } else {
+        if (txtEnable) log.info "Read Current: AutoOffOn will toggle is disabled"
+    }
+    if (!logEnable) log.info "Read Current: Debug must be enabled to see realtime switchvalue in Log. This has performance impact."
+    if (logEnable) {
+        pauseExecution(500)
+        switchvalue = device.currentValue("switch")
+        if (txtEnable) log.warn "Read Current: switchvalue is $switchvalue"
+    }
+}
+
 def initialize() {
-    if (!reversed) off()
-    if (reversed) on()
+    if (!reversed) {
+        if (txtEnable) log.info "INITIALIZE button pushed."
+        sendEvent(name: "switch", value: "off", isStateChange: true)
+        state.device = false
+        state.autoOffOnFired = false
+        if (txtEnable) log.info "${device.displayName} is initialized and Not Reversed"
+        if (txtEnable) log.info "Initialized: device is Not Reversed (normal)"
+        if (txtEnable) log.info "Initialized: device state is $state.device"
+        if (txtEnable) log.info "Initialized: switch is Off"
+        if (logEnable) log.info "Initialized: AutoOffOnFired is $state.autoOffOnFired"
+        if (autoOffOn.toInteger() > 0) {
+            if (txtEnable) log.info "Initialized: AutoOffOn is set to toggle in $autoOffOn seconds"
+        } else {
+            if (txtEnable) log.info "Initialized: AutoOffOn will not toggle"
+        }
+    } else {
+        if (txtEnable) log.info "INITIALIZE button pushed."
+        sendEvent(name: "switch", value: "on", isStateChange: true)
+        state.device = false
+        state.autoOffOnFired = false
+        if (txtEnable) log.info "${device.displayName} is initialized and is Reversed"
+        if (txtEnable) log.info "Initialized: device is REVERSED"
+        if (txtEnable) log.info "Initialized: device state is $state.device"
+        if (txtEnable) log.info "Initialized: switch is On"
+        if (logEnable) log.info "Initialized: AutoOffOnFired is $state.autoOffOnFired"
+        if (autoOffOn.toInteger() > 0) {
+            if (txtEnable) log.info "Initialized: AutoOffOn is set to toggle in $autoOffOn seconds"
+        } else {
+            if (txtEnable) log.info "Initialized: AutoOffOn will not toggle"
+        }
+    }
 }
--- Drivers/virtual_inverse-able_switch/virtual_inverse-able_w-autooff_switch.groovy
+++ Drivers/virtual_inverse-able_switch/virtual_inverse-able_w-autooff_switch.groovy
@@ -1,5 +1,5 @@
 /*
- * Virtual Inverse-able Switch
+ * Virtual Inverse-able w-autotoggle Switch
  *
  *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -16,10 +16,12 @@
  *      ----          ------        -------     ----                                              ---
  *      2021-10-10    jshimota      0.1.0       Starting version
  *      2021-10-11    jshimota      0.1.1       Added redundant set using parent and added initialize to follow preference so always starts in off position
+ *      2021-10-17    jshimota      0.1.2       Added Toggle feature
+ *      2021-10-20    jshimota      0.2.0       Added AutoOffOn - will toggle based on device setting in preferences
  * */
 metadata {
     definition(
-            name: "Virtual Inverse-able w-autooff Switch",
+            name: "Virtual Inverse-able w-autotoggle Switch",
             namespace: "jshimota",
             author: "James Shimota",
             importUrl: "https://raw.githubusercontent.com/jshimota01/hubitat/main/Drivers/virtual_inverse-able_switch/virtualInverse-ableSwitch.groovy"
@@ -27,16 +29,15 @@ metadata {
         capability "Actuator"
         capability "Switch"
         capability "Initialize"
-        capability "Refresh"
         command "toggle"
+        command "readCurrentIntoLog"
     }
 
     preferences {
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
         input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
         input name: "reversed", type: "bool", title: "Reverse Action", defaultValue: false, required: true
-        // input name: "forceUpdate", type: "bool", title: "Force State Update", description: "Send event everytime, regardless of current status. ie Send/Do On even if already On.",  defaultValue: false
-        input name: "autoOff", type: "enum", description: "Automatically turns off the device after selected time.", title: "Enable Auto-Off", options: [[0:"Disabled"],[1:"1s"],[2:"2s"],[5:"5s"],[10:"10s"],[20:"20s"],[30:"30s"],[60:"1m"],[120:"2m"],[300:"5m"],[1800:"30m"],[3200:"60m"]], defaultValue: 0
+        input name: "autoOffOn", type: "enum", description: "Automatically turns off -or- on the device after selected time.", title: "Enable Auto-Off-On", options: [[0:"Disabled"],[1:"1 second"],[2:"2 seconds"],[5:"5 seconds"],[10:"10 seconds"],[15:"15 seconds"],[20:"20 seconds"],[30:"30 seconds"],[45:"45 seconds"],[60:"1 minute"],[120:"2 minutes"],[300:"5 minutes"],[600:"10 minutes"],[900:"15 minutes"],[1200:"20 minutes"],[1800:"30 minutes"],[2700:"45 minutes"],[3200:"1 hour"]], defaultValue: 0
     }
 }
 
@@ -49,81 +50,125 @@ def logsOff() {
 }
 
 def on() {
+    sendEvent(name: "switch", value: "on", isStateChange: true)
     if (reversed) {
-        sendEvent(name: "switch", value: "off")
         state.device = false
         parent?.componentOff(this.device)
-        if (txtEnable) log.info "${device.displayName} turned ON but is reversed."
+        if (txtEnable) log.info "${device.displayName} switch turned ON - is reversed - device state is $state.device."
     } else {
-        sendEvent(name: "switch", value: "on")
         state.device = true
+        state.autoOffOnFired = false
         parent?.componentOn(this.device)
-        if (txtEnable) log.info "${device.displayName} turned ON but is NOT reversed."
+        if (txtEnable) log.info "${device.displayName} switch turned ON - is NOT reversed - device state is $state.device."
     }
     autotoggle()
 }
 
 def off() {
+    sendEvent(name: "switch", value: "off", isStateChange: true)
     if (reversed) {
-        sendEvent(name: "switch", value: "on")
         state.device = true
+        state.autoOffOnFired = false
         parent?.componentOn(this.device)
-        if (txtEnable) log.info "${device.displayName} turned OFF but is reversed."
+        if (txtEnable) log.info "${device.displayName} switch turned OFF - is reversed - device state is $state.device."
     } else {
-        sendEvent(name: "switch", value: "off")
         state.device = false
         parent?.componentOff(this.device)
-        if (txtEnable) log.info "${device.displayName} turned OFF but is NOT reversed."
+        if (txtEnable) log.info "${device.displayName} switch turned OFF - is NOT reversed - device state is $state.device."
     }
     autotoggle()
 }
 
 def autotoggle() {
-    if (autoOff.toInteger() > 0) {
-        runIn(autoOff.toInteger(), toggle)
+    if (logEnable) log.debug "autotoggle: AutoOffOnFired prerun is $state.autoOffOnFired"
+    if (!state.autoOffOnFired == true) {
+        if (logEnable) log.debug "autotoggle: Auto-Off-OnFired innotloop is $state.autoOffOnFired"
+        if (autoOffOn.toInteger() > 0) {
+            if (logEnable) log.debug "${device.displayName} will toggle in $autoOffOn seconds."
+            runIn(autoOffOn.toInteger(), toggle)
+            state.autoOffOnFired = true
+            if (logEnable) log.debug "autotoggle: Auto-Off-OnFired afterrun is $autoOffOnFired"
+        }
     }
 }
 
 def toggle() {
-    def switchvalue = device.currentValue("switch")
-    log.info "switch is ${state.device}"
-    log.info "switchvalue is $switchvalue"
-    log.info "reversed is $reversed"
-    //if (device.currentValue("switch") == "on") log.info "switch is on as toggle runs"
-    if ((reversed) && (device.currentValue("switch") == "on")) {
-        off()
+    if (reversed) {
+        if (state.device == true) {
+            on()
+        } else {
+            off()
+        }
+    } else {
+        if (!reversed) {
+            if (state.device == false) {
+                on()
+            } else {
+                off()
+            }
+        }
     }
-    if ((reversed) && (device.currentValue("switch") == "off")) {
-        on()
-    }
-    if ((!reversed) && (device.currentValue("switch") == "off")) {
-        on()
-    }
-    if ((!reversed) && (device.currentValue("switch") == "on")) {
-        off()
-    }
-}
-
-def refresh() {
-    initialize()
 }
 
 def updated(){
     log.info "${device.displayName} updated..."
     log.warn "${device.displayName} debug logging is: ${logEnable == true}"
     log.warn "${device.displayName} description logging is: ${txtEnable == true}"
+    initialize()
 }
 
 def installed() {
     initialize()
 }
 
+def readCurrentIntoLog() {
+    if (txtEnable) log.info "Read Current button pushed."
+    if (txtEnable) log.info "Read Current: device state is $state.device."
+    if (txtEnable) log.info "Read Current: device inverted? $reversed."
+    if (logEnable) log.info "Read Current: Auto-Off-OnFired is $state.autoOffOnFired"
+    if (autoOffOn.toInteger() > 0) {
+        if (txtEnable) log.info "Read Current: AutoOffOn will toggle in $autoOffOn seconds"
+    } else {
+        if (txtEnable) log.info "Read Current: AutoOffOn will toggle is disabled"
+    }
+    if (!logEnable) log.info "Read Current: Debug must be enabled to see realtime switchvalue in Log. This has performance impact."
+    if (logEnable) {
+        pauseExecution(500)
+        switchvalue = device.currentValue("switch")
+        if (txtEnable) log.warn "Read Current: switchvalue is $switchvalue"
+    }
+}
+
 def initialize() {
     if (!reversed) {
-        off()
-        if (txtEnable) log.info "${device.displayName} is initialized as OFF with no reversal"
+        if (txtEnable) log.info "INITIALIZE button pushed."
+        sendEvent(name: "switch", value: "off", isStateChange: true)
+        state.device = false
+        state.autoOffOnFired = false
+        if (txtEnable) log.info "${device.displayName} is initialized and Not Reversed"
+        if (txtEnable) log.info "Initialized: device is Not Reversed (normal)"
+        if (txtEnable) log.info "Initialized: device state is $state.device"
+        if (txtEnable) log.info "Initialized: switch is Off"
+        if (logEnable) log.info "Initialized: AutoOffOnFired is $state.autoOffOnFired"
+        if (autoOffOn.toInteger() > 0) {
+            if (txtEnable) log.info "Initialized: AutoOffOn is set to toggle in $autoOffOn seconds"
+        } else {
+            if (txtEnable) log.info "Initialized: AutoOffOn will not toggle"
+        }
     } else {
-        on()
-        log.info "${device.displayName} is initialized as ON but is reversed!"
+        if (txtEnable) log.info "INITIALIZE button pushed."
+        sendEvent(name: "switch", value: "on", isStateChange: true)
+        state.device = false
+        state.autoOffOnFired = false
+        if (txtEnable) log.info "${device.displayName} is initialized and is Reversed"
+        if (txtEnable) log.info "Initialized: device is REVERSED"
+        if (txtEnable) log.info "Initialized: device state is $state.device"
+        if (txtEnable) log.info "Initialized: switch is On"
+        if (logEnable) log.info "Initialized: AutoOffOnFired is $state.autoOffOnFired"
+        if (autoOffOn.toInteger() > 0) {
+            if (txtEnable) log.info "Initialized: AutoOffOn is set to toggle in $autoOffOn seconds"
+        } else {
+            if (txtEnable) log.info "Initialized: AutoOffOn will not toggle"
+        }
     }
 }
