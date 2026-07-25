1dccf824410b5481ce1ae4c9badc71b1c06f5231
Jim Shimota <jshimota01@hotmail.com>
2026-07-25 11:16:24 -0700

    fixed again

--- Drivers/owm_multi-API_weather/owm_multi-API_weather_v2.4.0.groovy
+++ Drivers/owm_multi-API_weather/owm_multi-API_weather_v2.4.0.groovy
@@ -13,7 +13,7 @@
 	on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 	for the specific language governing permissions and limitations under the License.
 	
-*/
+**/
 /**
 	This is a complete rewrite of the original driver done by James Shimota.  This driver is designed to fit 
 	a number of needs and does both Tile generation for dashboards AND attributes useful for functions within Hubitat Elevation Rules
